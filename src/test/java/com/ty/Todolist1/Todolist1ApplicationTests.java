8315e521e907088772c81dc416db8f8241043a59
HIZA <hizaokm@gmail.com>
2025-09-29 15:10:46 +0530

    To do list website

---
+++ src/test/java/com/ty/Todolist1/Todolist1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.Todolist1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Todolist1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
